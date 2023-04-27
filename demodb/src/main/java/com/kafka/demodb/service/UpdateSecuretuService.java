3d0d3b6fbbcf33992ffbb8699ca749b1bdde65f4
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-04-27 16:12:10 +0200

    checkotpv client service implementing

--- demodb/src/main/java/com/kafka/demodb/service/UpdateSecuretuService.java
+++ demodb/src/main/java/com/kafka/demodb/service/UpdateSecuretuService.java
@@ -1,6 +1,7 @@
 package com.kafka.demodb.service;
 
 import com.kafka.demodb.model.response.BaseDbResponse;
+import com.kafka.demodb.service.fragment.CheckOtpvService;
 import com.kafka.demodb.service.internal.SecCounterCrudService;
 import com.kafka.demodb.service.internal.UserSecCrudService;
 import org.springframework.stereotype.Service;
@@ -10,18 +11,21 @@ public class UpdateSecuretuService {
 
     private final SecCounterCrudService secCounterCrudService;
     private final UserSecCrudService userSecCrudService;
+    private final CheckOtpvService checkOtpvService;
 
-    public UpdateSecuretuService(SecCounterCrudService secCounterCrudService, UserSecCrudService userSecCrudService) {
+    public UpdateSecuretuService(SecCounterCrudService secCounterCrudService, UserSecCrudService userSecCrudService, CheckOtpvService checkOtpvService) {
         this.secCounterCrudService = secCounterCrudService;
         this.userSecCrudService = userSecCrudService;
+        this.checkOtpvService = checkOtpvService;
     }
 
     //TODO: tutti i servizi qui lavorano con otp
     // in caso di otp errato/ invio otp aumentare counterMail
     //certifiMail
-    public BaseDbResponse certifyMailUser(String userKey, String email, String opt){
+    public BaseDbResponse certifyMailUser(String userKey, String email, String opt,String trxId){
 
-        //TODO: integrare con servizio di checkOtp
+        //TODO: manca fragment e client che chiamano checkotpv, il microservizio gai esiste
+        //TODO: integrare con servizio di checkOtp, il microservices e stato creato, va solo integrato
         return new BaseDbResponse();
     }
     //changePsw
---
+++ demodb/src/main/java/com/kafka/demodb/service/fragment/CheckOtpvService.java
@@ -0,0 +1,7 @@
+package com.kafka.demodb.service.fragment;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CheckOtpvService {
+}
