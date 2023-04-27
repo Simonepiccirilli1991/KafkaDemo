7baa0ac46d761b1a34068adc5b35f918601cec15
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-04-27 12:35:13 +0200

    service full implemented e tested

--- otpv/src/main/java/com/kafka/otpv/controller/OtpvController.java
+++ otpv/src/main/java/com/kafka/otpv/controller/OtpvController.java
@@ -1,7 +1,39 @@
 package com.kafka.otpv.controller;
 
+import com.kafka.otpv.model.request.CheckOtpvRequest;
+import com.kafka.otpv.model.request.GenerateOtpvRequest;
+import com.kafka.otpv.model.response.CheckOtpvSummaryResponse;
+import com.kafka.otpv.service.CheckOtpvService;
+import com.kafka.otpv.service.GenerateOtpvService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeoutException;
+
 @RestController
+@RequestMapping("api/v1/otp")
 public class OtpvController {
+
+
+    private final CheckOtpvService checkOtpvService;
+    private final GenerateOtpvService generateOtpvService;
+
+    public OtpvController(CheckOtpvService checkOtpvService, GenerateOtpvService generateOtpvService) {
+        this.checkOtpvService = checkOtpvService;
+        this.generateOtpvService = generateOtpvService;
+    }
+
+
+    @PostMapping("/generate")
+    public ResponseEntity<GenerateOtpvService.GenerateOtpv> generateOtp(@RequestBody GenerateOtpvRequest request){
+        return ResponseEntity.ok(generateOtpvService.generateOtpv(request.getUserKey()));
+    }
+
+    @PostMapping("/check")
+    public ResponseEntity<CheckOtpvSummaryResponse> checkOtp(@RequestBody CheckOtpvRequest request) throws Exception {
+        return ResponseEntity.ok(checkOtpvService.checkOtp(request));
+    }
 }
